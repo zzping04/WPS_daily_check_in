84ff6881cbb6e48f060b23d9a14c67ed35dc3d9c
datugou <953274732@qq.com>
2020-11-18 16:23:16 +0800

    Create get_questionbank_answer.py

---
+++ get_questionbank_answer.py
@@ -0,0 +1,29 @@
+import requests
+
+wps_sid = 'V02StmNx2jDkYJGLQ1suV7tCWs4UGuI00a5b2e57000e955a0d'
+
+cookie = {
+    'wps_sid': wps_sid
+}
+
+s = requests.session()
+
+qs = ['1', '2', '3', '4', '1|2|3', '2|3|4', '1|2', '1|3', '1|4', '2|3', '2|4', '3|4', '1|2|3|4']
+
+for k in ('PC_task_knowledge', 'pc_task_konwMembers'):
+    questionbank = []
+    for j in range(11):
+        for i in qs:
+            verify_q = 'https://vip.wps.cn/questionbank/verify'
+            data ={
+                'task_tag': k,
+                'position': k,
+                'answers[%s]'%j: i
+            }
+            r = s.post(verify_q, cookies=cookie, data = data)
+            isOK = json.loads(r.text)['result']
+            if isOK == 'ok':
+                questionbank.append('%s:%s'%(j,i))
+                break
+    print(k)
+    print(questionbank)
