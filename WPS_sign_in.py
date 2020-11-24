b67b80895a250de9bd4623521334ee13835177e7
datugou <953274732@qq.com>
2020-11-24 10:25:10 +0800

    Update and rename WPS_check_in.py to WPS_sign_in.py

---
+++ WPS_sign_in.py
@@ -0,0 +1,18 @@
+import random, os, json
+import requests
+
+wps_sid = os.getenv('WPS_SID').strip()
+
+s = requests.session()
+question = 'https://vip.wps.cn/checkcode/signin/question'
+r = s.get(question, cookies = {'wps_sid': wps_sid})
+
+data = {
+        'platform': '2',
+        'answer': '1',
+        'auth_type': 'answer'
+}
+
+answer = 'https://vip.wps.cn/sigin/do'
+r = s.post(answer, cookies = {'wps_sid': wps_sid}, data = data)
+
