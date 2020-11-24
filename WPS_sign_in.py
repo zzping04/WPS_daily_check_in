6f72ab1730f4b9f980b81f1072bc384cab001853
datugou <953274732@qq.com>
2020-11-24 10:25:38 +0800

    Update WPS_sign_in.py

--- WPS_sign_in.py
+++ WPS_sign_in.py
@@ -7,11 +7,9 @@ s = requests.session()
 question = 'https://vip.wps.cn/checkcode/signin/question'
 r = s.get(question, cookies = {'wps_sid': wps_sid})
 
-data = {
-        'platform': '2',
+data = {'platform': '2',
         'answer': '1',
-        'auth_type': 'answer'
-}
+        'auth_type': 'answer'}
 
 answer = 'https://vip.wps.cn/sigin/do'
 r = s.post(answer, cookies = {'wps_sid': wps_sid}, data = data)
