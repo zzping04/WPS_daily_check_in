b30e17928acac2aa15de327d4750bc45ae2c3621
datugou <953274732@qq.com>
2020-11-25 09:34:57 +0800

    Update WPS_sign_in.py

--- WPS_sign_in.py
+++ WPS_sign_in.py
@@ -1,16 +1,19 @@
 import random, os, json
 import requests
 
-wps_sid = os.getenv('WPS_SID').strip()
+try:
+    wps_sid = os.getenv('WPS_SID').strip()
 
-s = requests.session()
-question = 'https://vip.wps.cn/checkcode/signin/question'
-r = s.get(question, cookies = {'wps_sid': wps_sid})
+    s = requests.session()
+    question = 'https://vip.wps.cn/checkcode/signin/question'
+    r = s.get(question, cookies = {'wps_sid': wps_sid})
 
-data = {'platform': '2',
-        'answer': '1',
-        'auth_type': 'answer'}
+    data = {'platform': '2',
+            'answer': '1',
+            'auth_type': 'answer'}
 
-answer = 'https://vip.wps.cn/sigin/do'
-r = s.post(answer, cookies = {'wps_sid': wps_sid}, data = data)
+    answer = 'https://vip.wps.cn/sigin/do'
+    r = s.post(answer, cookies = {'wps_sid': wps_sid}, data = data)
+except:
+    print('没有配置SID，如需积分签到，请到 settings->secrets 中配置 WPS_SID')
 
